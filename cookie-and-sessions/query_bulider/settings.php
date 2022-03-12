6ef4f045e87b53891f3340e70c0946150aaaf5ad
Shaileshprajapati-dev <95132308+Shaileshprajapati-dev@users.noreply.github.com>
2022-03-12 15:19:39 +0530

    Add files via upload

---
+++ cookie-and-sessions/query_bulider/settings.php
@@ -0,0 +1,22 @@
+<?php
+//This is Setting file
+
+################ All Constants ##########################
+define('HOST','localhost:3306');
+define('USER','root');
+define('PASSWORD','');
+define('DBNAME','app-22');
+
+################ All Constants ##########################
+
+return [
+	
+	'db:config'=>[
+		
+		'host'=>'localhost:3306',
+		'user'=>'root',
+		'password'=>'',
+		'dbname'=>'app-22',
+	],
+    'connection:debug' => false,
+];
---
+++ cookie-and-sessions/user/index.php
@@ -0,0 +1,9 @@
+User Dashboard
+<?php
+session_start();
+$validUser = $_SESSION['validUser'];
+
+?>
+<h1>Hi !!! <?php echo $validUser->email; ?></h1>
+<a href="logout.php">LogOut</a>
+?>
